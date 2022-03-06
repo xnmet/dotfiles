33bca07a41e3207eb2ca26e928da10d2c121a7cb
nmet <t@nmet.xyz>
2022-03-06 18:40:25 -0300

    Fix code style

--- .config/nvim/lua/plugged/hop.lua
+++ .config/nvim/lua/plugged/hop.lua
@@ -1,6 +1,6 @@
 require('hop').setup()
 
-nvim_map('nx', 's', "<cmd>lua require'hop'.hint_words({ multi_windows = true })<cr>")
-nvim_map('nxo', 'r', "<cmd>lua require'hop'.hint_char1({ multi_windows = true })<cr>")
-nvim_map('nxo', 'f', "<cmd>lua require'hop'.hint_lines_skip_whitespace({ multi_windows = true })<cr>")
-nvim_map('nxo', 'e', "<cmd>lua require'hop'.hint_words({ keys = 'asdjklqweuiocn', current_line_only = true })<cr>")
+nvim_map('nx', 's', "<cmd>lua require('hop').hint_words({ multi_windows = true })<cr>")
+nvim_map('nxo', 'r', "<cmd>lua require('hop').hint_char1({ multi_windows = true })<cr>")
+nvim_map('nxo', 'f', "<cmd>lua require('hop').hint_lines_skip_whitespace({ multi_windows = true })<cr>")
+nvim_map('nxo', 'e', "<cmd>lua require('hop').hint_words({ keys = 'asdjklqweuiocn', current_line_only = true })<cr>")
--- .config/nvim/lua/plugged/lualine.lua
+++ .config/nvim/lua/plugged/lualine.lua
@@ -2,14 +2,14 @@ require('lualine').setup({
   icons_enabled = false,
   options = {
     section_separators = '',
-    component_separators = ''
+    component_separators = '',
   },
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diagnostics' },
     lualine_c = { 'filename' },
-    lualine_x = {  },
+    lualine_x = { },
     lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_z = { 'location' },
   },
 })
